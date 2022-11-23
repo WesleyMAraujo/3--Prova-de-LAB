9d05e4ffa4be4b4321f98544110f23c1fe19b4af
WesleyMAraujo <wesleymonteirodearaujo@gmail.com>
2022-11-23 10:26:19 -0300

    so testes

--- ler_arquivo.cpp
+++ ler_arquivo.cpp
@@ -6,6 +6,10 @@
 int ler(){
     FILE *legenda_cordenada;
     legenda_cordenada = fopen("bancoDeDados/legenda-cordenada.txt", "w");
+    
+    
+    
+    
     fprintf(legenda_cordenada,"aaaaaaaa");
     fclose(legenda_cordenada);
     
