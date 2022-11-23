3a9110ad08442fad9f48ca86491972c9c2c984f8
WesleyMAraujo <wesleymonteirodearaujo@gmail.com>
2022-11-23 10:58:27 -0300

    a

--- ler_arquivo.cpp
+++ ler_arquivo.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+typedef struct {
+    char latitude[6];
+    char longitude[6];
+    char codigo[6];
+    char UF[2];
+    char nome_cidade[20];
+}cidades_juntas;
+
+
 /**
  * ler os arquivos de cordenadas e legenda
 */
@@ -7,7 +16,8 @@ int ler(){
     FILE *legenda_cordenada;
     legenda_cordenada = fopen("bancoDeDados/legenda-cordenada.txt", "w");
     
-    
+    FILE *ler_coordenadas;
+    ler_coordenadas = fopen("bancoDeDados/coordenadas.csv", "r");
     
     
     fprintf(legenda_cordenada,"aaaaaaaa");
