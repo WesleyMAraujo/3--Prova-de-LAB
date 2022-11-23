eaecb6adb384d9363048576f0734078468b1eeaf
WesleyMAraujo <wesleymonteirodearaujo@gmail.com>
2022-11-23 10:23:22 -0300

    iniciando

---
+++ ler_arquivo.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * ler os arquivos de cordenadas e legenda
+*/
+int ler(){
+    FILE *legenda_cordenada;
+    legenda_cordenada = fopen("bancoDeDados/legenda-cordenada.txt", "w");
+    fprintf(legenda_cordenada,"aaaaaaaa");
+    fclose(legenda_cordenada);
+    
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "ler_arquivo.cpp"
+
+
+    
+int main(){
+    ler();
+
+
+
+    return 0;
+}
